0ba9434f1906ffb68ad1c590937df7346b7ea472
kolberg <kolbergconsulting@gmail.com>
2016-12-09 14:45:23 -0500

    should work in offline and aws now

--- src/functions/pets/handler.ts
+++ src/functions/pets/handler.ts
@@ -1,19 +1,13 @@
 import path = require("path");
-
-if (process.env.IS_SERVERLESS || process.env.IS_OFFLINE) {
-  require("app-module-path").addPath(process.env.PWD + path.sep + "build");
-}
-
-import { Blah } from "src/lib/Blah";
-import { PetsRepo } from "src/functions/pets/lib/repository/PetsRepo";
-var repo = new PetsRepo(Blah);
-
+//require("app-module-path").addPath((process.env.LAMBDA_RUNTIME_DIR ? process.env.LAMBDA_RUNTIME_DIR : ".") + path.sep + "build");
+require("app-module-path").addPath("." + path.sep + "build");
+import {Blah} from 'src/lib/Blah'
 export function pets(event: any, context: any, callback: any) {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'This message is writen by TypeScript.',
-      input: event
+      input: process.env
     })
   };
 
