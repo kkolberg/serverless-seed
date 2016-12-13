2d22072f388f80f5ad10acc81e025f56dfbbb25a
kolberg <kolbergconsulting@gmail.com>
2016-12-13 17:19:31 -0500

    saves for session

--- src/functions/pets/lib/repository/localfilerepo.ts
+++ src/functions/pets/lib/repository/localfilerepo.ts
@@ -5,24 +5,31 @@ import fs = require('fs');
 var temp = require("temp").track();
 
 export class LocalFileRepo implements RepoInterface {
-    private name: string = "tempPets";
-    type: string =  "LocalFileRepo";
+
+    type: string = "LocalFileRepo";
+
+    getFile(): string {
+        if (process.env.petsFile) {
+            return process.env.petsFile;
+        }
+
+        process.env.petsFile = temp.openSync("tmpPets").path;
+        return process.env.petsFile;
+    }
 
     fetch(callback: Function) {
-        
-        temp.open(this.name, (err: any, info: any) => {
+        fs.readFile(this.getFile(), 'utf8', (err: any, data: any) => {
             if (err) {
                 return callback(err, null);
             }
 
-            fs.readFile(info.path, (err: any, data: any) => {
-                if (err) {
-                    return callback(err, null);
-                }
+            if (!data) {
+                return callback(null, []);
+            }
 
-                callback(null, <Array<Pet>>JSON.parse(data));
-            });
+            callback(null, <Array<Pet>>JSON.parse(data));
         });
+
     }
 
     save(pet: Pet, callback: Function) {
@@ -32,15 +39,12 @@ export class LocalFileRepo implements RepoInterface {
                 return callback(err, null);
             }
 
-            temp.open(this.name, (err: any, info: any) => {
-                if (err) {
-                    return callback(err, null);
-                }
+            pets.push(pet);
 
-                pets.push(pet);
-
-                fs.writeFile(info.path, pets);
+            fs.writeFile(this.getFile(), JSON.stringify(pets), 'utf8', (err: any) => {
+                callback(err, pets);
             });
         });
+
     }
 }
