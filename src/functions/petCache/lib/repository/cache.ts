b3a754389e0101a098281b1008fdc29296d98ed2
kolberg <kolbergconsulting@gmail.com>
2017-01-07 22:13:02 -0500

    renaming so easier to tell what the example is of

--- src/functions/petCache/lib/repository/cache.ts
+++
@@ -1,9 +0,0 @@
-import { Pet } from "src/functions/petCache/model/pet";
-
-export interface Cache {
-    put(pet: Pet, callback: PutCallback): void;
-    list(callback: ListCallback): void;
-}
-
-export type PutCallback = (err: any) => void;
-export type ListCallback = (err: any, pets: Pet[]) => void;
--- src/functions/petCache/model/pet.ts
+++
@@ -1,5 +0,0 @@
-export interface Pet {
-    name: string;
-    weight: number;
-    price: number;
-}
